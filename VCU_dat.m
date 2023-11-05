74ad920132914059a77c0e2233de0d114e8ff5cb
RainGOB <128049507+RainGOB@users.noreply.github.com>
2023-11-06 00:04:55 +0800

    before share,i need a little back up.

---
+++ VCU_dat.m
@@ -0,0 +1,15 @@
+%add calibration parameter Period1
+Period1=600;
+Period1=ecucoder_cal(Period1);
+
+%add calibration parameter Duty1
+Duty1=5000;
+Duty1=ecucoder_cal(Duty1);
+
+%add calibration parameter Period2
+Period2=600;
+Period2=ecucoder_cal(Period2);
+
+%add calibration parameter Duty2
+Duty2=5000;
+Duty2=ecucoder_cal(Duty2);
